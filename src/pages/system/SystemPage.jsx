f9b4e3737263f045b5d2cd0fcfbb572eb3f6ff4d
Felipe Ferreira de Paula <felipefp2@gmail.com>
2025-08-10 02:30:04 -0300

    Modal to New Order Created

--- src/pages/system/SystemPage.jsx
+++ src/pages/system/SystemPage.jsx
@@ -1,28 +1,42 @@
+import { useEffect, useRef, useState } from "react";
 import { Table } from "react-bootstrap";
 
 
 export default function SystemPage() {
+    const [newOrderModal, setNewOrderModal] = useState(false);
+    const newOrderModalRef = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (newOrderModalRef.current && !newOrderModalRef.current.contains(event.target)) {
+                setNewOrderModal(false);
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => document.removeEventListener("mousedown", handleClickOutside);
+    }, []);
 
     return (
         <>
             <div style={{ display: 'flex', flexDirection: 'column', height: '100%', width: '100%', alignContent: 'left', flexGrow: 1 }}>
                 <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '100%', height: '50px' }}>
-                    <button style={{ backgroundColor: '#333', border: "2px solid white", color: "white", padding: "10px 20px", marginBottom: '20px', height: '40px' }} onClick={() => window.location.reload()}>New Order</button>
+                    <button style={{ backgroundColor: 'rgba(22, 111, 163, 1)', border: "2px solid white", color: "white", padding: "10px 20px", marginBottom: '20px', height: '40px', marginLeft: '0px' }} onClick={() => setNewOrderModal(true)}>New Order</button>
                 </div>
 
                 <div style={{ display: 'flex', flexDirection: 'column', textAlign: 'left', alignContent: 'left', justifyItems: 'left', }}>
                     <h3 style={{ color: "white", marginBottom: '10px' }}>Orders Cooking</h3>
 
                     {/* <div style={{ display: 'flex', flexDirection: 'row', margin: '20px', backgroundColor: "white", color: "black", padding: '5px', borderRadius: '10px' }}> */}
-                    <Table responsive="sm" style={{ backgroundColor: "white", color: "black", borderRadius: '10px', marginBottom: '20px', padding: '10px', minWidth: '80%', minHeight: '200px', maxHeight: '300px', overflow: 'auto',}}>
+                    <Table responsive="sm" style={{ backgroundColor: "white", color: "black", borderRadius: '10px', marginBottom: '20px', padding: '10px', minWidth: '80%', height: '100%', minHeight: '200px', maxHeight: '400px', overflow: 'auto', }}>
                         <thead>
                             <tr>
-                                <th style={{ width: "40px"}}>Num</th>
+                                <th style={{ width: "40px" }}>Num</th>
                                 <th>Customer</th>
                                 <th>Minutes</th>
                             </tr>
                         </thead>
-                        <tbody>
+                        <tbody style={{ textAlign: 'left', height: '100%', maxHeight: '400px', overflowY: 'auto' }}>
                             <tr>
                                 <td>1</td>
                                 <td>Alexandro</td>
@@ -37,7 +51,16 @@ export default function SystemPage() {
                     </Table>
                     {/* </div> */}
                 </div>
-            </div>
+
+                {newOrderModal && <div style={{ position: 'absolute', display: 'flex', height: '100%', width: '100%', backgroundColor: 'rgba(0, 0, 0, 0.5)', alignItems: 'center', justifyContent: 'center', borderRadius: '10px', zIndex: 9 }} >
+                    <div ref={newOrderModalRef} style={{ display: 'flex', flexDirection: 'column', width: 400, border: '2px solid white', background: "linear-gradient(135deg, #272727ff, #18183aff)", color: 'white', padding: '20px', borderRadius: '10px', zIndex: 10 }}>
+                        <h1>New Order</h1>
+                        <h1>New Order</h1>
+                        <h1>New Order</h1>
+
+                    </div>
+                </div>}
+            </div >
         </>
     );
 }
