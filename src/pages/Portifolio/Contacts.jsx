fcdca8f1e1d391d2d3e479da1311941d88044e0c
Felipe Ferreira de Paula <felipefp2@gmail.com>
2025-10-03 01:48:47 -0300

    Contact Updated

--- src/pages/Portifolio/Contacts.jsx
+++ src/pages/Portifolio/Contacts.jsx
@@ -5,13 +5,26 @@ import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import mailLogo from '../../assets/mailLogo.png';
 import whatsAppLogo from '../../assets/whatsAppLogo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+
 
 export default function Contacts({ activeMenu }) {
     const isDesktopView = useSelector((state) => state.view.isDesktopView);
     const email = 'felipefp2@gmail.com';
     const phone = '+55 11 94731 - 5319';
+    const [copiedEmailMsg, setCopiedEmailMsg] = useState(null);
 
     const { t, i18n } = useTranslation();
+
+    useEffect(() => {
+        if (copiedEmailMsg) {
+            const timer = setTimeout(() => {
+                setCopiedEmailMsg(null);
+            }, 2000);
+            return () => clearTimeout(timer);
+        }
+    }, [copiedEmailMsg]);
 
     return (
         <>
@@ -21,15 +34,24 @@ export default function Contacts({ activeMenu }) {
                     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '20px', backgroundColor: 'rgba(255, 255, 255, 0.1)', borderRadius: '10px', height: '100%', justifyContent: 'center' }}>
                         <span style={{ fontWeight: 'bold', fontSize: isDesktopView ? '26px' : '26px', textDecoration: 'none' }}>{"Felipe Ferreira"}</span>
 
-                        <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', justifyItems: 'center', alignContent: 'center', alignItems: 'center', margin: '50px 0px', cursor: 'pointer' }}
-                            onClick={() => { window.open(`mailto:${email}`, "_blank"); }}>
+                        <div onClick={() => { isDesktopView ? (navigator.clipboard.writeText(email), setCopiedEmailMsg(t("copiedMsg"))) : window.open(`mailto:${email}`, "_blank"); }}
+                            style={{ position: 'relative', display: 'flex', flexDirection: 'row', justifyContent: 'center', justifyItems: 'center', alignContent: 'center', alignItems: 'center', margin: '50px 0px', cursor: 'pointer' }} >
+
                             <img src={mailLogo} alt="Logo" style={{ width: 70, borderRadius: '6px', marginRight: '10px' }} />
                             <span style={{ fontWeight: 'bold', fontSize: isDesktopView ? '22px' : '19px', marginBottom: '5px' }}>{email}</span>
                             {/* <span>{t("projects.myHealth.description")}</span> */}
+
+                            {copiedEmailMsg && <div style={{ display: 'flex', flexDirection: 'row', position: 'absolute', top: 60, padding: '20px 20px', backgroundColor: 'rgba(255, 255, 255, 0.3)', borderRadius: '6px', padding: '5px', 
+                                alignItems: 'center', animation: 'fadeInOut 20s' }}>
+
+                                <span style={{ margin: '5px 10px', fontSize: isDesktopView ? '18px' : '16px', color: 'white', fontWeight: 'bold' }}>{copiedEmailMsg}</span> 
+                                <FontAwesomeIcon icon={faCheck} style={{ color: 'rgba(30, 230, 183, 0.79)' }} />
+                            </div>}
                         </div>
 
-                        <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', justifyItems: 'center', alignContent: 'center', alignItems: 'center', margin: '50px 0px', cursor: 'pointer' }}
-                            onClick={() => window.open(`https://wa.me/${phone.replaceAll(" ", "").replaceAll("-", "")}`, "_blank")} >
+                        <div onClick={() => window.open(`https://wa.me/${phone.replaceAll(" ", "").replaceAll("-", "")}`, "_blank")}
+                            style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', justifyItems: 'center', alignContent: 'center', alignItems: 'center', margin: '50px 0px', cursor: 'pointer' }} >
+
                             <img src={whatsAppLogo} alt="Logo" style={{ width: 70, borderRadius: '6px', marginRight: '10px' }} />
                             <span style={{ fontWeight: 'bold', fontSize: isDesktopView ? '22px' : '19px', marginBottom: '5px' }}>{phone}</span>
                             {/* <span>{t("projects.myHealth.description")}</span> */}
