f2179af1e6dbf5209569e360c86a09c444256021
Felipe Ferreira de Paula <felipefp2@gmail.com>
2025-11-15 02:04:50 -0300

    Ajust on nav

--- src/pages/DeliverySite/companyOperation/deliverymanPage/DeliveryManPage.jsx
+++ src/pages/DeliverySite/companyOperation/deliverymanPage/DeliveryManPage.jsx
@@ -13,6 +13,7 @@ export default function DeliveryManPage({ companyOperation, getShiftOperationDat
     const theme = useSelector((state) => state.view.theme);
     const [ordersGroups, setOrdersGroups] = useState([]);
     const [sequenceOpen, setSequenceOpen] = useState(null);
+    const [showChoseRouteModal, setShowChoseRouteModal] = useState(false);
 
     useEffect(() => {
         separateOrdersOnGorups();
@@ -21,13 +22,9 @@ export default function DeliveryManPage({ companyOperation, getShiftOperationDat
     async function separateOrdersOnGorups() {
         const userOrders = companyOperation?.orders?.filter(order => order?.deliveryManID === localStorage.getItem('userLoggedEmail') && order?.status === 'CLOSEDWAITINGPAYMENT');
 
-        console.log('userOrders: ', userOrders);
-
         const uniqueDeliveryOrdersSequence = [
             ...new Set(userOrders?.map(o => JSON.stringify(o.deliveryOrdersSequence)))
         ].map(s => JSON.parse(s));
-
-        console.log('uniqueDeliveryOrdersSequence: ', uniqueDeliveryOrdersSequence);
 
         let ordersGroupsTemp = [];
         uniqueDeliveryOrdersSequence.forEach(sequenceArray => {
@@ -56,10 +53,9 @@ export default function DeliveryManPage({ companyOperation, getShiftOperationDat
         });
 
         setOrdersGroups(ordersGroupsTemp);
-        console.log('ordersGroupsTemp: ', ordersGroupsTemp);
     }
 
-    async function goMapsAutoStart(orders) {
+    async function goMaps(orders) {
         if (!orders || orders.length === 0) {
             console.error("Orders list cannot be empty for routing.");
             return;
@@ -78,34 +74,10 @@ export default function DeliveryManPage({ companyOperation, getShiftOperationDat
             googleMapsUrl += `&waypoints=${waypoints}`;
         }
 
-        // googleMapsUrl += `&dir_action=navigate&travelmode=driving`;
+        // googleMapsUrl += `&dir_action=navigate&travelmode=driving`; // That Starts navigation immediately
         googleMapsUrl += `&travelmode=driving`;
         window.open(googleMapsUrl, '_blank');
     }
-
-    // async function goMapsAutoStart(orders) {
-    //     if (!orders || orders.length === 0) {
-    //         console.error("Orders list cannot be empty for routing.");
-    //         return;
-    //     }
-    //     const getCoords = (order) => `${order?.customer?.lat},${order?.customer?.lng}`;
-
-    //     const waypoints = orders
-    //         .slice(0, -1)
-    //         .map(getCoords)
-    //         .join('|');
-    //     const destination = getCoords(orders[orders.length - 1]);
-
-    //     let googleMapsUrl = 'https://www.google.com/maps/dir/?api=1' +
-    //         `&destination=${destination}`;
-    //     if (waypoints.length > 0) {
-    //         googleMapsUrl += `&waypoints=${waypoints}`;
-    //     }
-
-    //     // googleMapsUrl += `&dir_action=navigate&travelmode=driving`;
-    //     googleMapsUrl += `&travelmode=driving`;
-    //     window.open(googleMapsUrl, '_blank');
-    // }
 
     async function goWaze(orders) {
         console.log('ordersGroups: ', ordersGroups);
@@ -113,32 +85,14 @@ export default function DeliveryManPage({ companyOperation, getShiftOperationDat
             console.error("Orders list cannot be empty for routing.");
             return;
         }
-
         const getCoords = (order) => `${order?.customer?.lat},${order?.customer?.lng}`;
 
         const origin = getCoords(orders[0]);
         const destination = getCoords(orders[orders.length - 1]);
 
-        // Waze URL format
         const wazeUrl = `https://waze.com/ul?ll=${destination}&from=${origin}&navigate=yes`;
-
         window.open(wazeUrl, '_blank');
     }
-
-    // function openMaps() {
-    //     // const url = `geo:${lat},${lng}?q=${lat},${lng}`;
-    //     let url = `https://www.google.com/maps/dir/?api=1&destination=${lat},${lng}&travelmode=driving`;
-
-    //     const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-
-    //     if (isSafari) {
-    //         url = `http://maps.apple.com/?daddr=${lat},${lng}&dirflg=d`;
-    //     }
-
-    //     window.open(url, '_blank'); // open in new tab
-    //     setGoToOutsideModalVisible(false)
-    // };
-
 
     return (
         <>
@@ -156,7 +110,7 @@ export default function DeliveryManPage({ companyOperation, getShiftOperationDat
                         </div>
                         {sequenceOpen === index && <div className='flexRow spaceBetweenJC' style={{ gap: 50, marginTop: 10 }} >
                             <img src={googleMapsLogo} alt="Google Maps" style={{ width: 50, height: 50, borderRadius: 10, backgroundColor: "white", border: "2px solid white" }}
-                                onClick={(e) => { e.stopPropagation(); goMapsAutoStart(group?.orders); }} />
+                                onClick={(e) => { e.stopPropagation(); goMaps(group?.orders); }} />
 
                             <img src={wazeLogo} alt="Waze" style={{ width: 50, height: 50, borderRadius: 10, backgroundColor: blueOne(theme), border: "2px solid white" }}
                                 onClick={(e) => { e.stopPropagation(); goWaze(group?.orders); }} />
