8e75034651075f469c45567533b39f1c77f2171e
Felipe Ferreira de Paula <felipefp2@gmail.com>
2025-12-01 03:49:03 -0300

    Almost Add addons and notes

--- src/pages/DeliverySite/companyOperation/system/components/auxComponents/2ndLevel/CustomItemModal.jsx
+++ src/pages/DeliverySite/companyOperation/system/components/auxComponents/2ndLevel/CustomItemModal.jsx
@@ -1,9 +1,10 @@
-import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { faCheck, faPeace, faPen } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { borderColorTwo, greenOne, greenTwo, purpleOne } from '../../../../../../../theme/Colors';
+import { borderColorTwo, fontColorOne, greenOne, greenTwo, purpleOne } from '../../../../../../../theme/Colors';
 import { getImageFoodService } from '../../../../../../../services/deliveryServices/auxServices/FoodsImagesService';
+import noFoodImg from '../../../../../../../assets/noFood.jpg';
 
 
 export default function CustomItemModal({ close, category, inputSearchItem, setInputSearchItem, productsFiltered, customSize, setSelectedCustomItems }) {
@@ -11,7 +12,20 @@ export default function CustomItemModal({ close, category, inputSearchItem, setI
     const isPcV = useSelector((state) => state.view.isPcV);
 
     const [itemsSelectedToCreateCustom, setItemsSelectedToCreateCustom] = useState([]);
+    const [addonsSelectedToCreateCustom, setAddonsSelectedToCreateCustom] = useState([]);
+    const [notesForCustomItem, setNotesForCustomItem] = useState("");
 
+    const [tasteOrAddons, setTasteOrAddons] = useState('taste');
+    const [itemsOrNotes, setItemsOrNotes] = useState('items');
+
+    const [inputSearchAddons, setInputSearchAddons] = useState("");
+    const [addonsFiltered, setAddonsFiltered] = useState([]);
+
+    useEffect(() => {
+        setAddonsFiltered(category.productOptions.filter(item =>
+            item.name.toUpperCase().includes(inputSearchAddons.toUpperCase())
+        ));
+    }, [inputSearchAddons, category.productOptions]);
 
     async function passToSelectedCustomItems(customItem) {
         setSelectedCustomItems(prev => [...prev, customItem]);
@@ -30,12 +44,16 @@ export default function CustomItemModal({ close, category, inputSearchItem, setI
 
     async function calculatePriceAsync() {
         return category.customOrderPriceRule === 'BIGGESTPRICE' ?
-            Math.max(...itemsSelectedToCreateCustom.map(item => item.price)) : itemsSelectedToCreateCustom.map(p => p.price).reduce((a, b) => a + b, 0) / itemsSelectedToCreateCustom.length;
+            Math.max(...itemsSelectedToCreateCustom.map(item => item.price)) + addonsSelectedToCreateCustom.map(addon => addon.price).reduce((a, b) => a + b, 0)
+            :
+            (itemsSelectedToCreateCustom.map(p => p.price).reduce((a, b) => a + b, 0) / itemsSelectedToCreateCustom.length) + addonsSelectedToCreateCustom.map(addon => addon.price).reduce((a, b) => a + b, 0);
     }
 
     function calculatePrice() {
         return category.customOrderPriceRule === 'BIGGESTPRICE' ?
-            Math.max(...itemsSelectedToCreateCustom.map(item => item.price)) : itemsSelectedToCreateCustom.map(p => p.price).reduce((a, b) => a + b, 0) / itemsSelectedToCreateCustom.length;
+            Math.max(...itemsSelectedToCreateCustom.map(item => item.price)) + addonsSelectedToCreateCustom.map(addon => addon.price).reduce((a, b) => a + b, 0)
+            :
+            (itemsSelectedToCreateCustom.map(p => p.price).reduce((a, b) => a + b, 0) / itemsSelectedToCreateCustom.length) + addonsSelectedToCreateCustom.map(addon => addon.price).reduce((a, b) => a + b, 0);
     }
 
     function removeItemFromSelection(item) {
@@ -54,61 +72,143 @@ export default function CustomItemModal({ close, category, inputSearchItem, setI
         });
     }
 
-        return (
-            <>
-                <div className='modalInside' style={{ width: !isPcV ? "100%" : "98%", maxHeight: '90%', padding: !isPcV ? '10px' : '20px', }}>
-                    <span style={{ color: borderColorTwo(theme), fontSize: isPcV ? '26px' : '20px', fontWeight: 'bold' }}>{category.categoryName + " - " + customSize + " Sabores"} </span>
-                    <br />
+    function removeAddonsFromSelection(item) {
+        setAddonsSelectedToCreateCustom(prev => {
+            const index = prev.findIndex(p => p.id === item.id);
 
-                    <div className='flexColumn' style={{ width: '100%', height: '300px', backgroundColor: 'rgba(255, 255, 255, 1)', borderRadius: '5px', border: `2px solid ${borderColorTwo(theme)}` }}>
-                        <input type="text" className='inputStandart' value={inputSearchItem} onChange={(e) => setInputSearchItem(e.target.value.toUpperCase())} placeholder="Filter Item"
-                            style={{ width: '100%', backgroundColor: 'white', color: 'black', boxShadow: '1px 2px 6px rgba(0, 0, 0, 0.1)' }} />
+            // If not found → add
+            if (index === -1) {
+                return [...prev, item];
+            }
 
-                        <div className='flexRow' style={{ justifyContent: 'center', width: '100%', flexWrap: 'wrap', overflowY: 'auto', }}>
-                            {productsFiltered && productsFiltered?.map((product, idx) => (
-                                <div key={idx} className='flexColumn' style={{ width: '80px', margin: 5, cursor: 'pointer', position: 'relative', }}
-                                    onClick={() => { if (itemsSelectedToCreateCustom.length < customSize) setItemsSelectedToCreateCustom([...itemsSelectedToCreateCustom, product]); }}>
+            // If found → remove only ONE
+            const newArr = [...prev];
+            newArr.splice(index, 1);
+            return newArr;
+        });
+    }
 
-                                    <img src={product?.imagePath ? getImageFoodService(product?.imagePath) : noFoodImg} alt={""} style={{ width: '80px', height: '80px', objectFit: 'cover', borderRadius: '5px' }} />
-                                    <span style={{ fontWeight: 'bold', fontSize: "16px", textAlign: 'center', color: 'black' }}>{product?.name}</span>
-                                    {itemsSelectedToCreateCustom?.includes(product) && <div className='flexRow fullCenter'
-                                        style={{
-                                            position: 'absolute', top: -5, left: -5, width: 25, height: 25, borderRadius: '50%', backgroundColor: purpleOne(theme),
-                                            border: '1px solid white', fontSize: 17, fontWeight: 'bold', color: 'white', boxShadow: '1px 2px 6px rgba(0, 0, 0, 0.52)'
-                                        }}>
-                                        <FontAwesomeIcon icon={faCheck} /></div>}
-                                </div>
-                            ))}
-                        </div>
+    return (
+        <>
+            <div className='modalInside' style={{ width: !isPcV ? "100%" : "98%", maxHeight: '90%', padding: !isPcV ? '10px' : '20px', }}>
+                <span style={{ color: borderColorTwo(theme), fontSize: isPcV ? '26px' : '20px', fontWeight: 'bold' }}>{category.categoryName + " - " + customSize + " Sabores"} </span>
+
+                {category?.productOptions?.length > 0 && <div className='flexRow spaceBetweenJC' style={{ marginBottom: 0 }} >
+                    <div className='flexRow fullCenter' style={{ width: '100%' }} >
+                        <span style={{ color: fontColorOne(theme), fontSize: 15, fontWeight: 'bold', cursor: 'pointer', opacity: tasteOrAddons === 'taste' ? 1 : 0.5, textDecoration: tasteOrAddons === 'taste' ? 'underline' : 'none' }}
+                            onClick={() => setTasteOrAddons('taste')}>{'Tastes'} </span>
                     </div>
 
+                    <div className='flexRow fullCenter' style={{ width: '100%' }} >
+                        <span style={{ color: fontColorOne(theme), fontSize: 15, fontWeight: 'bold', cursor: 'pointer', opacity: tasteOrAddons === 'addons' ? 1 : 0.5, textDecoration: tasteOrAddons === 'addons' ? 'underline' : 'none' }}
+                            onClick={() => setTasteOrAddons('addons')}>{'Add-ons'} </span>
+                    </div>
+                </div>}
 
-                    <div className='flexRow' style={{ marginTop: '10px', width: '100%', }}>
+                {tasteOrAddons === 'taste' && <div className='flexColumn' style={{ width: '100%', height: '300px', backgroundColor: 'rgba(255, 255, 255, 1)', borderRadius: '5px', border: `2px solid ${borderColorTwo(theme)}` }}>
+                    <input type="text" className='inputStandart' value={inputSearchItem} onChange={(e) => setInputSearchItem(e.target.value.toUpperCase())} placeholder="Filter Item"
+                        style={{ width: '100%', backgroundColor: 'white', color: 'black', boxShadow: '1px 2px 6px rgba(0, 0, 0, 0.1)' }} />
+
+                    <div className='flexRow' style={{ justifyContent: 'center', width: '100%', flexWrap: 'wrap', overflowY: 'auto', }}>
+                        {productsFiltered && productsFiltered?.map((product, idx) => (
+                            <div key={idx} className='flexColumn' style={{ width: '80px', margin: 5, cursor: 'pointer', position: 'relative', }}
+                                onClick={() => { if (itemsSelectedToCreateCustom.length < customSize) setItemsSelectedToCreateCustom([...itemsSelectedToCreateCustom, product]); }}>
+
+                                <img src={product?.imagePath ? getImageFoodService(product?.imagePath) : noFoodImg} alt={""} style={{ width: '80px', height: '80px', objectFit: 'cover', borderRadius: '5px' }} />
+                                <span style={{ fontWeight: 'bold', fontSize: "16px", textAlign: 'center', color: 'black' }}>{product?.name}</span>
+                                {itemsSelectedToCreateCustom?.includes(product) && <div className='flexRow fullCenter'
+                                    style={{
+                                        position: 'absolute', top: -5, left: -5, width: 25, height: 25, borderRadius: '50%', backgroundColor: purpleOne(theme),
+                                        border: '1px solid white', fontSize: 17, fontWeight: 'bold', color: 'white', boxShadow: '1px 2px 6px rgba(0, 0, 0, 0.52)'
+                                    }}>
+                                    <FontAwesomeIcon icon={faCheck} /></div>}
+                            </div>
+                        ))}
+                    </div>
+                </div>}
+                {tasteOrAddons === 'addons' && <div className='flexColumn' style={{ width: '100%', height: '300px', backgroundColor: 'rgba(255, 255, 255, 1)', borderRadius: '5px', border: `2px solid ${borderColorTwo(theme)}` }}>
+                    <input type="text" className='inputStandart' value={inputSearchAddons} onChange={(e) => setInputSearchAddons(e.target.value.toUpperCase())} placeholder="Filter Item"
+                        style={{ width: '100%', backgroundColor: 'white', color: 'black', boxShadow: '1px 2px 6px rgba(0, 0, 0, 0.1)' }} />
+
+                    <div className='flexRow' style={{ justifyContent: 'center', width: '100%', flexWrap: 'wrap', overflowY: 'auto', }}>
+                        {addonsFiltered && addonsFiltered?.map((addon, idx) => (
+                            <div key={idx} className='flexColumn' style={{ width: '80px', margin: 5, cursor: 'pointer', position: 'relative', }}
+                                onClick={() => { if (!addonsSelectedToCreateCustom?.includes(addon)) setAddonsSelectedToCreateCustom([...addonsSelectedToCreateCustom, addon]); }}>
+
+                                <img src={addon?.imagePath ? getImageFoodService(addon?.imagePath) : noFoodImg} alt={""} style={{ width: '80px', height: '80px', objectFit: 'cover', borderRadius: '5px' }} />
+                                <span style={{ fontWeight: 'bold', fontSize: "16px", textAlign: 'center', color: 'black' }}>{addon?.name}</span>
+                                {addonsSelectedToCreateCustom?.includes(addon) && <div className='flexRow fullCenter'
+                                    style={{
+                                        position: 'absolute', top: -5, left: -5, width: 25, height: 25, borderRadius: '50%', backgroundColor: purpleOne(theme),
+                                        border: '1px solid white', fontSize: 17, fontWeight: 'bold', color: 'white', boxShadow: '1px 2px 6px rgba(0, 0, 0, 0.52)'
+                                    }}>
+                                    <FontAwesomeIcon icon={faCheck} /></div>}
+                            </div>
+                        ))}
+                    </div>
+                </div>}
+
+                {category?.productOptions?.length > 0 && <div className='flexRow spaceBetweenJC' style={{ marginTop: 2 }} >
+                    <div className='flexRow fullCenter' style={{ width: '100%' }} >
+                        <span style={{ color: fontColorOne(theme), fontSize: 15, fontWeight: 'bold', cursor: 'pointer', opacity: itemsOrNotes === 'items' ? 1 : 0.5, textDecoration: itemsOrNotes === 'items' ? 'underline' : 'none' }}
+                            onClick={() => setItemsOrNotes('items')}>{'Items'} </span>
+                    </div>
+
+                    <div className='flexRow fullCenter' style={{ width: '100%' }} >
+                        <span style={{ color: fontColorOne(theme), fontSize: 15, fontWeight: 'bold', cursor: 'pointer', opacity: itemsOrNotes === 'notes' ? 1 : 0.5, textDecoration: itemsOrNotes === 'notes' ? 'underline' : 'none' }}
+                            onClick={() => setItemsOrNotes('notes')}><FontAwesomeIcon icon={faPen} /> {'Notes'} </span>
+                    </div>
+                </div>}
+
+                {itemsOrNotes === 'items' && <div className='flexRow' style={{ border: `1px solid ${borderColorTwo(theme)}`, borderRadius: 5, minHeight: 100 }}>
+                    <div className='flexRow' style={{ width: '100%', }}>
                         {itemsSelectedToCreateCustom && itemsSelectedToCreateCustom?.map((product, idx) => (
                             <div key={idx} className='flexColumn fullCenter' style={{ width: '80px', margin: 5, cursor: 'pointer', backgroundColor: 'rgba(240, 240, 240, 1)', borderRadius: 6, border: `2px solid ${borderColorTwo(theme)}`, position: 'relative' }}
                                 onClick={() => { removeItemFromSelection(product); }}>
                                 <img src={product?.imagePath ? getImageFoodService(product?.imagePath) : noFoodImg} alt={""} style={{ width: 40, height: 40, objectFit: 'cover', borderRadius: '5px' }} />
                                 <span style={{ fontWeight: 'bold', fontSize: "16px", textAlign: 'center', color: 'black' }}>{product?.name}</span>
-                                <div className='flexRow fullCenter'
+                                {/* <div className='flexRow fullCenter'
                                     style={{
                                         position: 'absolute', top: -10, left: -10, width: 25, height: 25, borderRadius: '50%', backgroundColor: purpleOne(theme),
                                         border: '1px solid white', fontSize: 17, fontWeight: 'bold', color: 'white', boxShadow: '1px 2px 6px rgba(0, 0, 0, 0.52)'
                                     }}>
-                                    {idx + 1}</div>
+                                    {idx + 1}</div> */}
                                 <span style={{ fontWeight: 'bold', fontSize: 15, textAlign: 'center', color: greenOne(theme) }}>{`$${product?.price.toFixed(2)}`}</span>
                             </div>
                         ))}
                     </div>
 
-                    <div className='flexRow spaceBetweenJC' style={{ marginTop: '10px', width: '100%' }}>
-                        <button className='buttonNoBgNoBorder' onClick={() => { close() }} > Cancel </button>
-
-                        <button className='buttonStandart fontGreenTwo' type="submit" style={{ opacity: (itemsSelectedToCreateCustom?.length === customSize) ? 1 : 0.2, cursor: (itemsSelectedToCreateCustom?.length === customSize) ? 'pointer' : 'not-allowed', }}
-                            disabled={itemsSelectedToCreateCustom?.length !== customSize} onClick={() => { handleCreateCustomItem(); }} >
-                            {itemsSelectedToCreateCustom?.length === customSize && <span style={{ fontWeight: 'bold', fontSize: 15, textAlign: 'center', color: greenTwo(theme) }}>{`$${calculatePrice().toFixed(2)} `}</span>}
-                            {itemsSelectedToCreateCustom?.length === customSize && <span style={{ fontWeight: 'bold', fontSize: 15, textAlign: 'center', color: greenTwo(theme), margin: '0px 1px' }}>{' | '}</span>}Add</button>
+                    <div className='flexRow' style={{ width: '100%', }}>
+                        {addonsSelectedToCreateCustom && addonsSelectedToCreateCustom?.map((product, idx) => (
+                            <div key={idx} className='flexColumn fullCenter' style={{ width: '80px', margin: 5, cursor: 'pointer', backgroundColor: 'rgba(240, 240, 240, 1)', borderRadius: 6, border: `2px solid ${borderColorTwo(theme)}`, position: 'relative' }}
+                                onClick={() => { removeAddonsFromSelection(product); }}>
+                                <img src={product?.imagePath ? getImageFoodService(product?.imagePath) : noFoodImg} alt={""} style={{ width: 40, height: 40, objectFit: 'cover', borderRadius: '5px' }} />
+                                <span style={{ fontWeight: 'bold', fontSize: "16px", textAlign: 'center', color: 'black' }}>{product?.name}</span>
+                                {/* <div className='flexRow fullCenter'
+                                    style={{
+                                        position: 'absolute', top: -10, left: -10, width: 25, height: 25, borderRadius: '50%', backgroundColor: purpleOne(theme),
+                                        border: '1px solid white', fontSize: 17, fontWeight: 'bold', color: 'white', boxShadow: '1px 2px 6px rgba(0, 0, 0, 0.52)'
+                                    }}>
+                                    {idx + 1}</div> */}
+                                <span style={{ fontWeight: 'bold', fontSize: 15, textAlign: 'center', color: greenOne(theme) }}>{`$${product?.price.toFixed(2)}`}</span>
+                            </div>
+                        ))}
                     </div>
+                </div>}
+
+                {itemsOrNotes === 'notes' && <div className='flexColumn' style={{ width: '100%', }}>
+                    <textarea className='textAreaStandart' rows={4} style={{ width: '100%' }} value={notesForCustomItem} onChange={(e) => setNotesForCustomItem(e.target.value)} />
+                </div>}
+
+                <div className='flexRow spaceBetweenJC' style={{ marginTop: '10px', width: '100%' }}>
+                    <button className='buttonNoBgNoBorder' onClick={() => { close() }} > Cancel </button>
+
+                    <button className='buttonStandart fontGreenTwo' type="submit" style={{ opacity: (itemsSelectedToCreateCustom?.length === customSize) ? 1 : 0.2, cursor: (itemsSelectedToCreateCustom?.length === customSize) ? 'pointer' : 'not-allowed', }}
+                        disabled={itemsSelectedToCreateCustom?.length !== customSize} onClick={() => { handleCreateCustomItem(); }} >
+                        {itemsSelectedToCreateCustom?.length === customSize && <span style={{ fontWeight: 'bold', fontSize: 15, textAlign: 'center', color: greenTwo(theme) }}>{`$${calculatePrice().toFixed(2)} `}</span>}
+                        {itemsSelectedToCreateCustom?.length === customSize && <span style={{ fontWeight: 'bold', fontSize: 15, textAlign: 'center', color: greenTwo(theme), margin: '0px 1px' }}>{' | '}</span>}Add</button>
                 </div>
-            </>
-        );
-    }
+            </div>
+        </>
+    );
+}
